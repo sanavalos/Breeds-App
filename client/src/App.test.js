8d8aa7385d45e00111efa4e0aaf5ef02efef4122
sanavalos <santiago.avalos@outlook.com>
2022-08-18 14:54:34 -0300

    breeds app ready to deploy

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/all dogs are good boys/i);
   expect(linkElement).toBeInTheDocument();
 });
