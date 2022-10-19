81badc401b3e15a54f33f600fb575641e41eccde
sanavalos <santiago.avalos@outlook.com>
2022-10-19 20:05:55 -0300

    fix(db, app): Railway deploy

--- api/src/db.js
+++ api/src/db.js
@@ -2,14 +2,13 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DIALECT } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
+  `${DB_DIALECT}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/dogs`,
   {
     logging: false,
     native: false,
-    port: Number("0.0.0.0"),
   }
 );
 const basename = path.basename(__filename);
