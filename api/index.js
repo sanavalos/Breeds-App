eb2b98d9f105399e91a36b2dbe1de428ddd5b113
sanavalos <santiago.avalos@outlook.com>
2022-10-20 18:52:40 -0300

    fix(index,db): railway deploy update

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
+const { PORT } = process.env;
 
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
+  server.listen(PORT, () => {
     console.log("%s listening at 3001");
   });
 });
--- api/src/db.js
+++ api/src/db.js
@@ -2,10 +2,11 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+const { DB_NAME, DB_DIALECT, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } =
+  process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/dogs`,
+  `${DB_DIALECT}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   {
     logging: false,
     native: false,
